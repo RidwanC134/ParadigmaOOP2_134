d67471dfe56664ca4dd322c7d5d9224a8d79cebb
RidwanC134 <ridwanhh10@gmail.com>
2023-05-29 11:01:07 +0700

    Membuat virtual public orang agar orang tidak muncul 2x

--- solveMultiWaris_134/solveMultiWaris_134.cpp
+++ solveMultiWaris_134/solveMultiWaris_134.cpp
@@ -5,4 +5,15 @@ class orang {
 public:
 	int umur;
 
+	orang(int pUmur) :
+		umur(pUmur)
+		{
+		cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+	}
+};
+
+class pekerja : virtual public orang {
+public:
+
+
 };
