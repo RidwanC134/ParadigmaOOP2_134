149911a52a869a0bcf379cc9a818488660a9bbae
RidwanC134 <ridwanhh10@gmail.com>
2023-05-29 10:48:28 +0700

    Membuat multi waris budi dari pekerja dan pelajar

--- ParadigmaOOP2_134/ParadigmaOOP2_134.cpp
+++ ParadigmaOOP2_134/ParadigmaOOP2_134.cpp
@@ -24,4 +24,16 @@ public:
 
 class pelajar : public orang {
 public:
+
+	pelajar(int pUmur) :
+		orang(pUmur)
+		{
+		cout << "pelajar dibuat\n" << endl;
+	}
+};
+
+class budi : public pekerja, public pelajar{
+public:
+
+
 };
