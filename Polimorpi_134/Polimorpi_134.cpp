ff0f23b9c0bcc5bf1d86a12f5adc4c38ba6fc1f5
RidwanC134 <ridwanhh10@gmail.com>
2023-05-29 11:17:52 +0700

    Membuat multi waris joko dari seseorang

--- Polimorpi_134/Polimorpi_134.cpp
+++ Polimorpi_134/Polimorpi_134.cpp
@@ -4,4 +4,12 @@ using namespace std;
 class seseorang {
 public:
 	virtual void pesan() = 0;
+	// virtual void pesan(){
+	//	cout << "Pesan dari seseorang" << endl;
+	// }
+};
+
+class joko :public seseorang {
+public:
+
 };
